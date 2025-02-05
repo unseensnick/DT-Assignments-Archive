122beaae369fa5fa363ecab097a82a4f34fbeea7
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-05 13:27:32 +0100

    javascript basic - datatypes

---
+++ DigitaleTalenter/Webutvikling-2/Javascript-Basic/datatypes/script.js
@@ -0,0 +1,62 @@
+const print = console.log;
+
+let name = "Ola";
+let city = "Oslo";
+let hobby = "Fotball";
+
+let age = 25;
+let height = 180;
+
+let person1 = {
+    name: "John",
+    age: 28,
+    city: "Oslo"
+};
+
+let person2 = {
+    name: "Kari",
+    age: 30,
+    city: "Bergen"
+};
+
+let fruits = ["Eple", "Banan", "Appelsin", "Druer"];
+let cars = ["Toyota", "BMW", "Tesla", "Ford"];
+
+// custom print function
+
+print(name)
+print(city)
+print(hobby)
+print(age)
+print(height)
+print(person1)
+print(person1.name)
+print(person1.age)
+print(person1.city)
+print(person2)
+print(person2.name)
+print(person2.age)
+print(person2.city)
+print(fruits)
+print(cars)
+
+
+// standard console log
+
+/*
+console.log(name);
+console.log(city);
+console.log(hobby);
+console.log(age);
+console.log(height);
+console.log(person1);
+console.log(person1.name);
+console.log(person1.age);
+console.log(person1.city);
+console.log(person2);
+console.log(person2.name);
+console.log(person2.age);
+console.log(person2.city);
+console.log(fruits);
+console.log(cars);
+*/
