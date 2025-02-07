aaf8c3b8fa1dfbbca3f9cc57b4b2d39549d82a2b
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-07 12:39:05 +0100

    Javascript Basic Assignment 2: Variables & Conditionals

---
+++ DigitaleTalenter/Webutvikling-2/Javascript-Basic/Variables & Conditionals/javascript.js
@@ -0,0 +1,142 @@
+/******************************************************************************
+OPPGAVE 1
+
+Din første oppgave er å koble denne JavaScript-filen til index.html-filen
+ved å bruke en av metodene vi viste i første forelesning.
+
+<-- Finn index.html-filen i filutforskeren og koble den til denne filen,
+javascript.js
+******************************************************************************/
+
+// Løs denne oppgaven i index.html
+
+/******************************************************************************
+OPPGAVE 2
+
+I forrige undervisning lærte vi hvordan man lager variabler som kan holde ulike
+typer verdier. Lag noen variabler med følgende datatyper:
+- String (tekst)
+- Number (tall)
+- Boolean (sann/usann)
+- Array (liste)
+
+Du kan velge hva innholdet i variablene skal være. Prøv å bruke både let og 
+const når du definerer variablene.
+******************************************************************************/
+
+// Skriv koden for oppgave 2 her
+let firstName = "John";
+let age = 18;
+let isAnAdult = age >= 18;
+let cars = ["Toyota", "BMW", "Tesla", "Ford"];
+
+console.log(firstName);
+console.log(age);
+console.log(cars);
+console.log(isAnAdult);
+
+/******************************************************************************
+OPPGAVE 3
+
+Prøv ut noen av operatorene vi så på i forrige forelesning:
+- Matematiske operatorer: +, -, /, *
+- Forkortede operatorer: ++, --, +=, -=
+
+Skriv noen eksempler der du tester disse operatorene.
+******************************************************************************/
+
+// Skriv koden for oppgave 3 her
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+let x = 5;
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+x += 3;
+console.log(x);
+
+x -= 2;
+console.log(x);
+
+let num = 10;
+
+num *= 2;
+console.log(num);
+
+num /= 4;
+console.log(num);
+
+/******************************************************************************
+OPPGAVE 4
+
+Skriv en IF/ELSE-betingelse som sjekker følgende:
+1. At userName ikke er tom ("").
+2. At userAge er 18 eller eldre.
+3. At userIsBlocked er false.
+
+(TIPS: Bruk && (logisk OG) for å sjekke alle tre betingelsene i én IF-setning.)
+
+- Hvis alle disse betingelsene er oppfylt, skal du sette variabelen
+userIsLoggedIn til true og goToPage til "/home". Deretter skriver du ut en 
+velkomstmelding med console.log.
+
+- Hvis noen av betingelsene IKKE er oppfylt, skal du skrive ut en feilmelding
+med console.log.
+
+Prøv å endre verdiene på variablene for å sikre at IF/ELSE-setningen din 
+håndterer alle tilfeller korrekt.
+******************************************************************************/
+
+let userName = "";
+let userAge = 18;
+let userIsLoggedIn = false;
+let userIsBlocked = false;
+let goToPage = "";
+
+// Skriv koden for oppgave 4 her
+if (userName !== "" && userAge >= 18 && userIsBlocked === false) {
+    userIsLoggedIn = true;
+    goToPage = "/home";
+    console.log(`Velkommen, ${userName}! Du blir sendt til ${goToPage}.`);
+} else {
+    console.log("Feil: Brukeren oppfyller ikke kravene for innlogging.");
+}
+
+/******************************************************************************
+OPPGAVE 5
+
+Lag en variabel kalt userTitle og sett innholdet til å være:
+- "Mr." hvis userMale er true, eller
+- "Mrs." hvis userMale er false.
+
+Bruk en ternary conditional for dette:
+
+const variabel = betingelse ? "hvis sann" : "hvis usann";
+
+Prøv å endre userMale til både true og false og bruk console.log for å sjekke
+at betingelsen din fungerer som den skal.
+******************************************************************************/
+
+const userMale = false;
+
+// Skriv koden for oppgave 5 her
+const userTitle = userMale ? "Mr." : "Mrs.";
+
+console.log(userTitle);
+
+function getUserTitle(userMale) {
+    return userMale ? "Mr." : "Mrs.";
+}
+
+console.log(getUserTitle(true));
+console.log(getUserTitle(false));
