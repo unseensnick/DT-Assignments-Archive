04b480e8d99f94a11db47e8828d3fefc95d957e6
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-21 21:56:37 +0100

    feat: implement dynamic playground page routing and configuration

---
+++ app/Webutvikling-2/Javascript-Basic/config.js
@@ -0,0 +1,38 @@
+export const playgroundConfigs = {
+    "week-1": {
+        datatypes: {
+            consoleWidth: 35,
+            animationDuration: 500,
+            animationEasing: "ease-in-out",
+            folders: ["datatypes"],
+        },
+        "variables-and-conditionals": {
+            consoleWidth: 40,
+            animationDuration: 500,
+            animationEasing: "ease-out",
+            folders: ["variables-and-conditionals"],
+        },
+    },
+    "week-2": {
+        functions: {
+            consoleWidth: 35,
+            animationDuration: 500,
+            animationEasing: "ease-in-out",
+            folders: ["functions"],
+        },
+        methods: {
+            consoleWidth: 40,
+            animationDuration: 500,
+            animationEasing: "ease-in-out",
+            folders: ["methods"],
+        },
+    },
+    "week-3": {
+        loops: {
+            consoleWidth: 30,
+            animationDuration: 500,
+            animationEasing: "ease-in-out",
+            folders: ["loops"],
+        },
+    },
+};
