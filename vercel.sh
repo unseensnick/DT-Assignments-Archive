b5e5c24ecf3fc1594ade77763afe65ef6b881106
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-24 01:52:26 +0100

    update vercel script

--- vercel.sh
+++ vercel.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Get the commit message of the latest commit
-commit_message=$(git log -1 --pretty=%B)
-
-# Check if the commit message contains the word "build"
-if [[ "$commit_message" == *"build"* ]]; then
-  echo "Commit message contains 'build'. Proceeding with the build..."
-  exit 0  # Allow the build to continue
+if [[ $VERCEL_ENV == "production"  ]] ; then 
+  git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
 else
-  echo "No 'build' in the commit message. Skipping the build..."
-  exit 1  # Prevent the build
-fi 
+  exit 1
+fi
