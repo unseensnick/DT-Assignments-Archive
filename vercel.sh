cf91ada0343114bed593364dd0cad5a341a371a9
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-24 01:56:49 +0100

    Revert "update vercel script"
    This reverts commit b5e5c24ecf3fc1594ade77763afe65ef6b881106.

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if [[ $VERCEL_ENV == "production"  ]] ; then 
-  git log -1 --pretty=oneline --abbrev-commit | grep -w "build" && exit 1 || exit 0
+# Get the commit message of the latest commit
+commit_message=$(git log -1 --pretty=%B)
+
+# Check if the commit message contains the word "build"
+if [[ "$commit_message" == *"build"* ]]; then
+  echo "Commit message contains 'build'. Proceeding with the build..."
+  exit 0  # Allow the build to continue
 else
-  exit 1
-fi
+  echo "No 'build' in the commit message. Skipping the build..."
+  exit 1  # Prevent the build
+fi 
