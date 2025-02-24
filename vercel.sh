19f8c1637443b88b1ebbb61e39e655e50e0b0121
unseensnick <84652498+unseensnick@users.noreply.github.com>
2025-02-24 01:43:31 +0100

    update vercel script

--- vercel.sh
+++ vercel.sh
@@ -6,8 +6,8 @@ commit_message=$(git log -1 --pretty=%B)
 # Check if the commit message contains the word "build"
 if [[ "$commit_message" == *"build"* ]]; then
   echo "Commit message contains 'build'. Proceeding with the build..."
-  exit 0 # Continue with the build process
+  exit 0  # Allow the build to continue
 else
   echo "No 'build' in the commit message. Skipping the build..."
-  exit 1 # Skip the build process
+  exit 1  # Prevent the build
 fi
